b75e730dbdbe6274a1bbcb3353d55626b2cdb501
vinhtuongluong <vinhtuongluong@gmail.com>
2025-06-02 12:44:02 +0700

    init frontend

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "antd";
+
+
+export default function Home() {
+  return (
+    <div>Hello World
+      <div>
+        <Button type="primary">Add new</Button>
+      </div>
+    </div>
+  );
+}
